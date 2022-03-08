cae9ec38b3430aa1766da86364fba2b8d944ba03
ViktorAtanasof <90446164+ViktorAtanasof@users.noreply.github.com>
2022-03-08 16:36:26 +0200

    Create numbersFromMtoN.js

---
+++ Basic Syntax, Conditional Statements and Loops - Lab/numbersFromMtoN.js
@@ -0,0 +1,9 @@
+function numbersFromMtoN(numM, numN) {
+    for (let i = numM; i >= numN; i--) {
+        console.log(i);
+    }
+}
+
+numbersFromMtoN(6, 2);
+numbersFromMtoN(4, 1);
+numbersFromMtoN(8, 4);
