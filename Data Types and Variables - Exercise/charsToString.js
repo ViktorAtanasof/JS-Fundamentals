fc088cf81746ab94a3e53b141ee83496cc4b6678
ViktorAtanasof <90446164+ViktorAtanasof@users.noreply.github.com>
2022-03-18 10:24:34 +0200

    Create charsToString.js

---
+++ Data Types and Variables - Exercise/charsToString.js
@@ -0,0 +1,8 @@
+function charsToString(char1, char2, char3) {
+    let charstoString = char1.toString() + char2.toString() + char3.toString();
+    console.log(charstoString);
+}
+
+charsToString('a', 'b', 'c');
+charsToString('%', '2', 'o');
+charsToString('1', '5', 'p');
