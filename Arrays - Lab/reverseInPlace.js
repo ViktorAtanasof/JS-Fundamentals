b16667daf909a6d664dfa8d008c0fa108e8113b6
ViktorAtanasof <90446164+ViktorAtanasof@users.noreply.github.com>
2022-03-25 09:30:31 +0200

    Create reverseInPlace.js

---
+++ Arrays - Lab/reverseInPlace.js
@@ -0,0 +1,8 @@
+function reverseInPlace(arr) {
+    let reversedArr = arr.reverse();
+    console.log(reversedArr.join(" "));
+}
+
+reverseInPlace(['a', 'b', 'c', 'd', 'e']);
+reverseInPlace(['abc', 'def', 'hig', 'klm', 'nop']);
+reverseInPlace(['33', '123', '0', 'dd']);
