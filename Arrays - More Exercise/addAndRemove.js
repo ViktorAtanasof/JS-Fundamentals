19a18ce2b1a170ce7ab3daf9170ad54ad7215828
ViktorAtanasof <90446164+ViktorAtanasof@users.noreply.github.com>
2022-03-30 11:50:25 +0300

    Create addAndRemove.js

---
+++ Arrays - More Exercise/addAndRemove.js
@@ -0,0 +1,23 @@
+function addAndRemove(arr) {
+    let newArray = [];
+    let index = 0;
+    let command = arr[index++];
+
+    for (let i = 1; i <= arr.length; i++) {
+        if (command === "add") {
+            newArray.push(Number(i));
+        }
+        if (command === "remove") {
+            newArray.pop();
+        }
+        command = arr[index++];
+    }
+    if (newArray.length <= 0) {
+        console.log("Empty");
+    }
+    console.log(newArray.join(" "));
+}
+
+addAndRemove(['add', 'add', 'add', 'add']);
+addAndRemove(['add', 'add', 'remove', 'add', 'add']);
+addAndRemove(['remove', 'remove', 'remove']);
