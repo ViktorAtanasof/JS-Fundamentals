82ef01491e0e7cdf493a6ac489982f5dc38e7018
ViktorAtanasof <90446164+ViktorAtanasof@users.noreply.github.com>
2022-03-31 11:42:42 +0300

    Create mathPower.js

---
+++ Functions - Lab/mathPower.js
@@ -0,0 +1,10 @@
+function mathPower(baseNum, exponentNum) {
+    function result(num1, num2) {
+        let finalResult = Math.pow(num1, num2);
+        return finalResult;
+    }
+    console.log(result(baseNum, exponentNum));
+}
+
+mathPower(2, 8);
+mathPower(3, 4);
