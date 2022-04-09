f08ffdb4c1eeb4bba6220594b8c937f7e0b1b77e
ViktorAtanasof <90446164+ViktorAtanasof@users.noreply.github.com>
2022-04-09 17:26:53 +0300

    Create smallestOfThreeNumbers.js

---
+++ Functions - Exercise/smallestOfThreeNumbers.js
@@ -0,0 +1,8 @@
+function smallestOfThreeNumbers(int1, int2, int3) {
+    console.log(Math.min(int1, int2, int3));
+}
+
+smallestOfThreeNumbers(2, 5, 3);
+smallestOfThreeNumbers(600, 342, 123);
+smallestOfThreeNumbers(25, 21, 4);
+smallestOfThreeNumbers(2, 2, 2);
