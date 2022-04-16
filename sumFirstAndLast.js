5e7ab4d9939e3241b9391709055ed31261a1c08a
ViktorAtanasof <90446164+ViktorAtanasof@users.noreply.github.com>
2022-04-16 15:30:38 +0300

    Delete sumFirstAndLast.js

--- sumFirstAndLast.js
+++
@@ -1,9 +0,0 @@
-function sumFirstAndLast(arr) {
-    let arrToNum = arr.map(Number);
-    let firstNum = arrToNum.shift();
-    let secondNum = arrToNum.pop();
-    console.log(firstNum + secondNum);
-}
-
-sumFirstAndLast(['20', '30', '40']);
-sumFirstAndLast(['5', '10']);
