9dd2389f5471ad8ad57bdd5ab6be0985b3aa9735
ViktorAtanasof <90446164+ViktorAtanasof@users.noreply.github.com>
2022-04-18 17:00:51 +0300

    Update distincArray.js

--- Arrays Advanced - Exercise/distincArray.js
+++ Arrays Advanced - Exercise/distincArray.js
@@ -4,6 +4,7 @@ function distincArray(arrOfNums) {
     })
     console.log(filteredArray.join(' '));
 }
+
 distincArray([1, 2, 3, 4]);
 distincArray([7, 8, 9, 7, 2, 3, 4, 1, 2]);
 distincArray([20, 8, 12, 13, 4, 4, 8, 5]);
