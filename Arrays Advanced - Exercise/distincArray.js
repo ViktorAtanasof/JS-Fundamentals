ffea023a04d178667613272f9f7b4c76ed1979fe
ViktorAtanasof <90446164+ViktorAtanasof@users.noreply.github.com>
2022-04-18 17:00:01 +0300

    Create distincArray.js

---
+++ Arrays Advanced - Exercise/distincArray.js
@@ -0,0 +1,9 @@
+function distincArray(arrOfNums) {
+    let filteredArray = arrOfNums.filter((number, index) => {
+        return arrOfNums.indexOf(number) === index
+    })
+    console.log(filteredArray.join(' '));
+}
+distincArray([1, 2, 3, 4]);
+distincArray([7, 8, 9, 7, 2, 3, 4, 1, 2]);
+distincArray([20, 8, 12, 13, 4, 4, 8, 5]);
