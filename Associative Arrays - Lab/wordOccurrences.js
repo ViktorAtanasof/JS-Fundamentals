61d43c1cd81867d8ff25d678cf6148eda2dc21ed
ViktorAtanasof <90446164+ViktorAtanasof@users.noreply.github.com>
2022-04-30 16:36:22 +0300

    Create wordOccurrences.js

---
+++ Associative Arrays - Lab/wordOccurrences.js
@@ -0,0 +1,23 @@
+function wordOccurrences(arrOfStrings) {
+    let map = new Map();
+    let words = 0;
+
+    for (let word of arrOfStrings) {
+        if (!map.has(word)) {
+            words = 1;
+        } else {
+            words = map.get(word) + 1;
+        }
+        map.set(word, words);
+    }
+    let sorted = Array.from(map);
+    sorted.sort((a, b) => b[1] - a[1]);
+
+    for (let [word, words] of sorted) {
+        console.log(`${word} -> ${words} times`);
+    }
+}
+
+wordOccurrences(["Here", "is", "the", "first", "sentence", "Here", "is", "another", "sentence", "And", "finally",
+    "the", "third", "sentence"]);
+wordOccurrences(["dog", "bye", "city", "dog", "dad", "boys", "ginger"]);
