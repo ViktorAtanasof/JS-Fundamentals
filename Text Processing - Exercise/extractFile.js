cb7ad941d58451c8a87ba3c3d88fecff58766de3
ViktorAtanasof <90446164+ViktorAtanasof@users.noreply.github.com>
2022-05-08 13:33:35 +0300

    Create extractFile.js

---
+++ Text Processing - Exercise/extractFile.js
@@ -0,0 +1,11 @@
+function extractFile(path) {
+    let fileAndExtension = path.split('\\');
+    let splittedFile = fileAndExtension.pop().split('.');
+    let extension = splittedFile.pop();
+
+    console.log(`File name: ${splittedFile.join('.')}`);
+    console.log(`File extension: ${extension}`);
+}
+
+extractFile('C:\\Internal\\training-internal\\Template.pptx');
+extractFile('C:\\Projects\\Data-Structures\\LinkedList.cs');
