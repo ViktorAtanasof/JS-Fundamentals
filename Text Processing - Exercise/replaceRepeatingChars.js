51b12e3b9547862e6e06d75154236cce52ed7794
ViktorAtanasof <90446164+ViktorAtanasof@users.noreply.github.com>
2022-05-18 11:25:31 +0300

    Create replaceRepeatingChars.js

---
+++ Text Processing - Exercise/replaceRepeatingChars.js
@@ -0,0 +1,14 @@
+function replaceRepeatingChars(string) {
+    let result = '';
+
+    for (let char of string) {
+        if (char !== result[result.length - 1]) {
+            result += char;
+        }
+    }
+
+    console.log(result);
+}
+
+replaceRepeatingChars('aaaaabbbbbcdddeeeedssaa');
+replaceRepeatingChars('qqqwerqwecccwd');
